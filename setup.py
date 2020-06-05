df64542f3caac9822c0231b715617c70e9b105d3
Emmett <ewlahay@ncsu.edu>
2020-06-05 13:46:59 -0400

    Add install files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='VoterLookup',
+	version='1.0',
+	packages=['Models'],
+	url='',
+	license='',
+	author='Emmett',
+	author_email='',
+	description=''
+)
